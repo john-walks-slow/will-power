760024f6b158342ebe1179837742a511454fc332
JohnnRen <john.ren.contact@gmail.com>
2019-05-25 18:53:35 +0800

    Build favicon at frontend

--- back_end/src/app.js
+++ back_end/src/app.js
@@ -30,9 +30,11 @@ app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
-// Host the public folder
-app.use('/', express.static(app.get('public')));
+// Host the public folder only in production
+if (process.env === 'production') {
+  app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+  app.use('/', express.static(app.get('public')));
+}
 
 // Set up Plugins and providers
 app.configure(express.rest());
